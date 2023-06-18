8087c948b2d94efd055c962da9a1531e2bd85c6a
Anuoluwapo Netufo <netufoanuoluwapo@gmail.com>
2023-06-18 13:53:43 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,20 +5,16 @@
  * Return: Always 0 (Success)
  */
 
-int main(void)
-{
-int lowerCase = 'a';
-while (lowerCase <= 'z')
-{
-if (lowerCase == 'e' || lowerCase == 'q')
-lowerCase += 1;
-}
-else
-{
-putchar(lowerCase);
-lowerCase += 1;
-}
-}
-putchar('\n');
-return (0);
+int main(void) {
+  char ch;
+
+  for (ch = 'a'; ch <= 'z'; ch++) {
+    if (ch != 'q' && ch != 'e') {
+      putchar(ch);
+    }
+  }
+
+  putchar('\n');
+
+  return 0;
 }
