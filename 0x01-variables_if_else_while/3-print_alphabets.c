79dd66e8f973d552791d6ce1f187ed554ed23145
Anuoluwapo Netufo <netufoanuoluwapo@gmail.com>
2023-06-18 14:35:14 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,22 +7,21 @@
 
 int main(void)
 {
-    char lowercase = 'a';
-    char uppercase = 'A';
+char lowercase = 'a';
+char uppercase = 'A';
 
-    while (lowercase <= 'z')
-    {
-        putchar(lowercase);
-        lowercase++;
-    }
+while (lowercase <= 'z')
+{
+putchar(lowercase);
+lowercase++;
+}
 
-    while (uppercase <= 'Z')
-    {
-        putchar(uppercase);
-        uppercase++;
-    }
+while (uppercase <= 'Z')
+{
+putchar(uppercase);
+uppercase++;
+}
 
-    putchar('\n');
-
-    return 0;
+putchar('\n');
+return (0);
 }
