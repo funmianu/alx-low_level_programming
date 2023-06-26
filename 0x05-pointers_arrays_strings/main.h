a6d05f965722f63e953608e19fab1b8f575e1f21
Anuoluwapo Netufo <netufoanuoluwapo@gmail.com>
2023-06-26 13:46:13 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <unistd.h>
-
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -12,5 +11,3 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
-#endif
