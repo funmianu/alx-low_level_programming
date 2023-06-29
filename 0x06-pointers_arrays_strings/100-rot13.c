bf14a1e39038afbd87ceda181204bfe38ff14d36
Anuoluwapo Netufo <netufoanuoluwapo@gmail.com>
2023-06-29 13:22:00 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using ROT13.
+ * @str: The string to encode.
+ *
+ * Return: A pointer to the encoded string.
+ */
+char *rot13(char *str)
+{
+int i, j;
+char *alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char *rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+for (i = 0; str[i] != '\0'; i++)
+{
+for (j = 0; alpha[j] != '\0'; j++)
+if (str[i] == alpha[j])
+{
+str[i] = rot13[j];
+break;
+}
+}
+}
+
+return (str);
+}
+
