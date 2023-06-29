4d3e4776d74987a31674a6736814c1dc0f687329
Anuoluwapo Netufo <netufoanuoluwapo@gmail.com>
2023-06-29 13:39:44 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -14,6 +14,7 @@ char *rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 for (i = 0; str[i] != '\0'; i++)
 {
 for (j = 0; alpha[j] != '\0'; j++)
+{
 if (str[i] == alpha[j])
 {
 str[i] = rot13[j];
