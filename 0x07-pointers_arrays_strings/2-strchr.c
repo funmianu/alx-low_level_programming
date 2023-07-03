8a03e728e5f84ca46c13009f9cf8ead0fc292d29
Anuoluwapo Netufo <netufoanuoluwapo@gmail.com>
2023-07-03 14:27:34 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ *
+ * Return: Always 0 (success)
+ */
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+return (s);
+s++;
+}
+if (*s == c)
+return (s);
+return (0);
+}
