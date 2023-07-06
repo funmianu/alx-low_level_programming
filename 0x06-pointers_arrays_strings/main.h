18dc3f46eef435568626ec74004d8111fb0b08b2
Anuoluwapo Netufo <netufoanuoluwapo@gmail.com>
2023-07-06 03:21:29 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,8 +10,6 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *str);
 char *leet(char *str);
-void magic_print(void);
-void magic_print(void);
 char *rot13(char *str);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
