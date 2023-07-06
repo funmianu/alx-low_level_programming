8c125a908fb15a790e61763a5058e4681fde585d
Anuoluwapo Netufo <netufoanuoluwapo@gmail.com>
2023-07-06 06:54:41 -0700

    0-puts_recursion.c

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -1,35 +1,12 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * simple_print_buffer - Prints a buffer
- * @buffer: The buffer to be printed
- * @size: The size of the buffer
+ * main - check the code
  *
- * Return: Nothing
+ * Return: Always 0.
  */
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-unsigned int i;
-
-for (i = 0; i < size; i++)
-{
-if (i % 10)
-{
-printf(" ");
-}
-if (!(i % 10) && i)
-{
-printf("\n");
-}
-printf("0x%02x", buffer[i]);
-}
-printf("\n");
-}
-
 int main(void)
 {
-char buffer[98] = "Hello, World! This is a buffer.";
-simple_print_buffer(buffer, 98);
+_puts_recursion("Puts with recursion");
 return (0);
 }
