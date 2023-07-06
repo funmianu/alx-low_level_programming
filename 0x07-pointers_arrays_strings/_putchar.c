7c57ceccb6c94e9c3b0d13f246fa5f59ce4aaaee
Anuoluwapo Netufo <netufoanuoluwapo@gmail.com>
2023-07-06 06:38:13 -0700

    0-puts_recursion.c

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,13 +1,9 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to the standard output
- * @c: the character to be written
- *
- * Return: On success 1
- * On error, returns -1 and sets errno appropriately.
+ * This is my _putcahr.c
  */
 int _putchar(char c)
 {
-return (write(1, &c, 1));
+return write(STDOUT_FILENO, &c, 1);
 }
